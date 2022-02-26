bf098b943f549b25346553397009832f1270729c
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-02-26 16:46:34 +0100

    Convert to function

--- formats/td4/phone_iantsa.c
+++ formats/td4/phone_iantsa.c
@@ -16,30 +16,16 @@
 static gnuplot_ctrl* h; // Plot graph.
 static fftw_plan plan; // FFT plan.
 
-
 // Correspondance table.
-static double line[4] = {697., 770., 852., 941.};
-static double col[3] = {1209., 1336., 1477.};
+static double line[4] = { 697., 770., 852., 941. };
+static double col[3] = { 1209., 1336., 1477. };
 
-static char c_tab[4][3] =
-{
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'},
-    {'*', '0', '#'}
+static char c_tab[4][3] = {
+    { '1', '2', '3' },
+    { '4', '5', '6' },
+    { '7', '8', '9' },
+    { '*', '0', '#' }
 };
-
-
-/**
- * @brief Prints usage on the console.
- *
- * @param progname The program name entered in command line.
- */
-static void
-usage(char* progname)
-{
-    fprintf(stderr, "Usage: %s <input file>.\n", progname);
-}
 
 /**
  * @brief Fills the frame buffer from the hop buffer.
@@ -160,15 +146,15 @@ fft_exit()
 /**
  * @brief Prints the number of local maxima in amplitude spectrum.
  * Note: first and last sample cannot be local maxima.
- * 
- * @param amp 
+ *
+ * @param amp
  */
 static void
 display_nb_peaks(double amp[FRAME_SIZE])
 {
     int n_peaks = 0;
     for (int i = 1; i < FRAME_SIZE - 1; i++)
-        if (amp[i] >= amp[i-1] && amp[i] > amp[i+1])
+        if (amp[i] >= amp[i - 1] && amp[i] > amp[i + 1])
             n_peaks++;
 
     printf("%d peaks found\n", n_peaks);
@@ -176,11 +162,11 @@ display_nb_peaks(double amp[FRAME_SIZE])
 
 /**
  * @brief Estimates peak frequency from previous and next samples values.
- * 
+ *
  * @param amp
  * @param sample
  * @param SAMPLE_RATE
- * 
+ *
  * @return The peak frequency computed.
  */
 static double
@@ -196,8 +182,8 @@ parabolic_interpolation(double amp[FRAME_SIZE], int sample, int SAMPLE_RATE)
 /**
  * @brief Computes the frequences corresponding to the 2 local maxima.
  * Note: first and last sample cannot be local maxima.
- * 
- * @param amp 
+ *
+ * @param amp
  * @param freq1
  * @param freq2
  * @param SAMPLE_RATE
@@ -206,15 +192,12 @@ static void
 retrieve_2_freq(double amp[FRAME_SIZE], double* freq1, double* freq2, int SAMPLE_RATE)
 {
     int peak1_sample = 0, peak2_sample = 0;
-    for (int i = 1; i < FRAME_SIZE/2 - 1; i++)
-        if (amp[i] >= amp[i-1] && amp[i] > amp[i+1])
-        {
-            if (amp[i] > amp[peak1_sample])
-            {
+    for (int i = 1; i < FRAME_SIZE / 2 - 1; i++)
+        if (amp[i] >= amp[i - 1] && amp[i] > amp[i + 1]) {
+            if (amp[i] > amp[peak1_sample]) {
                 peak2_sample = peak1_sample;
                 peak1_sample = i;
-            } 
-            else if (amp[i] > amp[peak2_sample])
+            } else if (amp[i] > amp[peak2_sample])
                 peak2_sample = i;
         }
 
@@ -241,18 +224,18 @@ hann(double n)
 
 /**
  * @brief Finds the index of the first occurrence of an element in an list of double.
- * 
+ *
  * @param list
  * @param n size of the list
  * @param el element wanted in the list
- * 
+ *
  * @return The index found.
- */ 
+ */
 static int
 indexOf(double* list, int n, double el)
 {
     for (int i = 0; i < n; i++)
-        if (list[i] == el) 
+        if (list[i] == el)
             return i;
     printf("Element not in list\n");
     exit(EXIT_FAILURE);
@@ -262,7 +245,7 @@ static int
 nearest_freq_index(double freq, double prec)
 {
     int n;
-    double *list;
+    double* list;
 
     if (freq < 1000)
         list = line, n = 4;
@@ -270,7 +253,7 @@ nearest_freq_index(double freq, double prec)
         list = col, n = 3;
 
     for (int i = 0; i < n; i++)
-        if (list[i]-prec <= freq && freq <= list[i]+prec)
+        if (list[i] - prec <= freq && freq <= list[i] + prec)
             return i;
 
     printf("Error: %lf is an invalid frequence\n", freq);
@@ -279,10 +262,10 @@ nearest_freq_index(double freq, double prec)
 
 /**
  * @brief Finds the key corresponding to a signal with frequences freq1 and freq2.
- * 
+ *
  * @param freq1
  * @param freq2
- * 
+ *
  * @return The char corresponding.
  */
 static char
@@ -303,7 +286,7 @@ decode(double freq1, double freq2)
 
 /**
  * @brief Computes energy of the signal on a portion in buffer.
- * 
+ *
  * @param buffer
  */
 static double
@@ -311,35 +294,22 @@ energy(double buffer[FRAME_SIZE])
 {
     double e = 0;
     for (int i = 0; i < FRAME_SIZE; i++)
-        e += buffer[i]*buffer[i];
+        e += buffer[i] * buffer[i];
     return e / FRAME_SIZE;
 }
 
-int main(int argc, char** argv)
+static void
+phone(char* infilename)
 {
     // Init file accessing.
-    char *progname, *infilename;
     SNDFILE* infile = NULL;
     SF_INFO sfinfo;
-
-    // Retrieve program name.
-    progname = strrchr(argv[0], '/');
-    progname = progname ? progname + 1 : argv[0];
-
-    // Check correct usage.
-    if (argc != 2) {
-        usage(progname);
-        return 1;
-    }
-
-    // Retrieve input file name.
-    infilename = argv[1];
 
     // Open input file.
     if ((infile = sf_open(infilename, SFM_READ, &sfinfo)) == NULL) {
         fprintf(stderr, "Not able to open input file %s.\n", infilename);
         puts(sf_strerror(NULL));
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     // Init file reading.
@@ -357,14 +327,14 @@ int main(int argc, char** argv)
             fill_buffer(buffer, new_buffer);
         else {
             fprintf(stderr, "Not enough samples.\n");
-            return EXIT_FAILURE;
+            exit(EXIT_FAILURE);
         }
     }
 
     // Retrieve file info.
     const unsigned int SAMPLE_RATE = sfinfo.samplerate; // 44100 Hz.
     const unsigned char NUM_CHANNELS = sfinfo.channels; // 1 (mono).
-    const unsigned int SIZE = (int)sfinfo.frames; 
+    const unsigned int SIZE = (int)sfinfo.frames;
 
     // Display file info.
     printf("Sample Rate: %d.\n", SAMPLE_RATE);
@@ -431,11 +401,10 @@ int main(int argc, char** argv)
         char key = decode(freq1, freq2);
 
         // // Check if we are analysing the same key as previous frame.
-        if (prev_key == key)
-        {
+        if (prev_key == key) {
             if (is_prev_silence)
                 printf("%c", key);
-                
+
         } else
             printf("%c", key);
 
@@ -462,5 +431,27 @@ int main(int argc, char** argv)
     // Shut down FFT, close file and exit program.
     fft_exit();
     sf_close(infile);
+}
+
+int main(int argc, char** argv)
+{
+    printf("--- \"sounds/telbase.wav\" ---\n");
+    phone("sounds/telbase.wav");
+
+    printf("\n--- \"sounds/telA.wav\" ---\n");
+    phone("sounds/telA.wav");
+
+    printf("\n--- \"sounds/telB.wav\" ---\n");
+    phone("sounds/telB.wav");
+
+    printf("\n--- \"sounds/telC.wav\" ---\n");
+    phone("sounds/telC.wav");
+
+    printf("\n--- \"sounds/telD.wav\" ---\n");
+    phone("sounds/telD.wav");
+
+    printf("\n--- \"sounds/telE.wav\" ---\n");
+    phone("sounds/telE.wav");
+
     return EXIT_SUCCESS;
 }
