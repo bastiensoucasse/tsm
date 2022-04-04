0ac661761a190b47c5f16c474bdec3672aa95c76
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-04-04 18:26:30 +0200

    Fix and test midi_bastien

--- midi/midi_bastien.c
+++ midi/midi_bastien.c
@@ -3,20 +3,20 @@
 
 #include "midifile.h"
 
-static char* notes[12] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+static const char* const notes[12] = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+static MidiFile_t md;
 
 static void
 usage(char* progname)
 {
-    fprintf(stderr, "Usage: %s <file>.\n", progname);
+    fprintf(stderr, "Usage: %s <input> <output> <transpose_value>.\n", progname);
     exit(EXIT_FAILURE);
 }
 
 static void
-read_midi(char* midifile)
+read_midi()
 {
-    MidiFile_t md = MidiFile_load(midifile);
-
     MidiFileEvent_t event = MidiFile_getFirstEvent(md);
     while (event) {
         long time = MidiFileEvent_getTick(event);
@@ -24,19 +24,19 @@ read_midi(char* midifile)
             printf("\nNote at %ld:\n", time);
 
             // Note
-            char* note = notes[MidiFileNoteStartEvent_getNote(event) % 12];
+            const char* const note = notes[MidiFileNoteStartEvent_getNote(event) % 12];
             printf("  - Note: %s.\n", note);
 
             // Velocity
-            int velocity = MidiFileNoteStartEvent_getVelocity(event);
+            const int velocity = MidiFileNoteStartEvent_getVelocity(event);
             printf("  - Velocity: %d.\n", velocity);
 
             // Duration
-            int duration = MidiFileEvent_getTick(MidiFileNoteStartEvent_getNoteEndEvent(event)) - MidiFileEvent_getTick(event);
+            const int duration = MidiFileEvent_getTick(MidiFileNoteStartEvent_getNoteEndEvent(event)) - MidiFileEvent_getTick(event);
             printf("  - Duration: %d.\n", duration);
 
             // Channel
-            int channel = MidiFileNoteStartEvent_getChannel(event);
+            const int channel = MidiFileNoteStartEvent_getChannel(event);
             printf("  - Channel: %d.\n", channel);
         }
 
@@ -45,17 +45,30 @@ read_midi(char* midifile)
 }
 
 static void
-transpose(char* input_filename, char* output_filename, int transpose)
+transpose(const int transpose_value)
 {
-    MidiFile_t input = MidiFile_load(input_filename);
+    MidiFileEvent_t event = MidiFile_getFirstEvent(md);
+    while (event) {
+        if (MidiFileEvent_isNoteStartEvent(event) && MidiFileNoteEndEvent_getChannel(event) != 10) {
+            const int note = MidiFileNoteStartEvent_getNote(event);
+            MidiFileNoteStartEvent_setNote(event, note + transpose_value);
+        }
+
+        event = MidiFileEvent_getNextEventInFile(event);
+    }
 }
 
 int main(int argc, char** argv)
 {
     if (argc != 4)
         usage(argv[0]);
+    
+    md = MidiFile_load(argv[1]);
 
-    transpose(argv[1], argv[2], atoi(argv[3]));
+    // read_midi(argv[1]);
 
+    transpose(atoi(argv[3]));
+
+    MidiFile_save(md, argv[2]);
     return EXIT_SUCCESS;
 }
